57014e399c380766ba496fc904f2596c98f8c590
martin <telemagico@gmail.com>
2018-11-14 19:29:49 -0300

    node data

---
+++ mercedes-dealers.js
@@ -0,0 +1,66 @@
+module.exports = [
+	{
+		"nombre":"Sucrusal 1",
+		"email": "sucursal1@gmail.com",
+		"telefono": "01112311231",
+		"whatsapp": "01112311231",
+		"vehicle_types":
+		[
+			"autos",
+			"comerciales"
+		],
+		"service_types":
+		[
+			"servicios",
+			"parabrisas"
+		]
+	},
+	{
+		"nombre":"Sucrusal 2",
+		"email": "sucursal2@gmail.com",
+		"telefono": "01112311231",
+		"whatsapp": "01112311231",
+		"vehicle_types":
+		[
+			"autos",
+			"comerciales"
+		],
+		"service_types":
+		[
+			"servicios",
+			"seguros"
+		]
+	},
+	{
+		"nombre":"Sucrusal 3",
+		"email": "sucursal3@gmail.com",
+		"telefono": "01112311231",
+		"whatsapp": "01112311231",
+		"vehicle_types":
+		[
+			"autos",
+			"vans"
+		],
+		"service_types":
+		[
+			"servicios",
+			"parabrisas"
+		]
+	},
+	{
+		"nombre":"Sucrusal 4",
+		"email": "sucursal4@gmail.com",
+		"telefono": "01112314444",
+		"whatsapp": "01112314444",
+		"vehicle_types":
+		[
+			"autos",
+			"vans"
+		],
+		"service_types":
+		[
+			"servicios",
+			"reposiciones"
+		]
+	}
+];
