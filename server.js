b2e0096ae975316c5f1b82c4a80d75c9bf416713
martin <telemagico@gmail.com>
2018-12-27 13:19:16 -0300

    recovered version

--- server.js
+++ server.js
@@ -1,35 +1,16 @@
 #!/usr/bin/env node
+
 var express = require('express');
 var path = require('path');
 var _ = require('lodash');
 var cors = require('cors')
 var bodyParser = require('body-parser');
 var app = express();
-var mercedes_dealers = require('./mercedes-dealers');
-var mercedes_models = require('./mercedes-models');
 
 // Set static files
 app.use(express.static('dist'));
 app.use(bodyParser.json());
 app.use(cors())
-
-app.post('/mercedes-dealers/:keyword',function(req, res){
-	var keyword = req.params.keyword.toLowerCase();
-	var results = _.filter(mercedes_dealers, function(item){
-	  return item.nombre.toLowerCase().indexOf(keyword) > -1 || item.nombre.toLowerCase().indexOf(keyword) > -1;
-	});
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(results));	
-});
-
-app.post('/mercedes-models/:keyword',function(req, res){
-	var keyword = req.params.keyword.toLowerCase();
-	var results = _.filter(mercedes_models, function(item){
-	  return item.title.toLowerCase().indexOf(keyword) > -1;
-	});
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(results));	
-});
 
 app.get('*', function(req, res){
 	res.sendFile('index.html', { root: __dirname + '/dist' });
